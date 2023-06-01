5b95283f060dd722e860b46be988d7753bd7ec13
Sidddha <nikitaprudov@gmail.com>
2023-06-01 17:37:00 +0300

    fix typo

--- tgbot/utils/db_api/schemas/user.py
+++ tgbot/utils/db_api/schemas/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, BigInteger, String, sql
+from sqlalchemy import Column, BigInteger, String, sql, Integer
 from tgbot.utils.db_api.db_gino import TimedBaseModel
 from sqlalchemy.dialects import postgresql
 
@@ -7,7 +7,7 @@ class User(TimedBaseModel):
     __tablename__ = 'users'
     id = Column(BigInteger, primary_key=True)
     name = Column(String(100))
-    phone = Column(int)
+    phone = Column(Integer)
     status = Column(String(15))
     address = Column(String(150))
     geo = Column(postgresql.POINT())
